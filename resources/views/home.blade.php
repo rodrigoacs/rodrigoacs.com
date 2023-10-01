825a128fa2ba2a5cf6a54819b52e86a74d7a2d4c
Rodrigo <rodrigohths@gmail.com>
2023-10-01 17:24:15 -0300

    chore: increasing animation speed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,19 +38,19 @@
     <!-- About-->
     <section class="resume-section print" id="about">
       <div class="resume-section-content">
-        <h1 class="mb-0" data-aos="fade-right">
+        <h1 class="mb-0" data-aos-duration="150" data-aos="fade-down">
           Rodrigo
           <span class="text-primary">Soares</span>
         </h1>
-        <div class="subheading mb-5" data-aos="fade-right">
+        <div class="subheading mb-5" data-aos-duration="150" data-aos="fade-down">
           Minas Gerais, Brasil · <a href="mailto:rodrigohths@gmail.com">rodrigohths@gmail.com</a>
         </div>
-        <p class="lead mb-5" data-aos="fade-right">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto eum
+        <p class="lead mb-5" data-aos-duration="150" data-aos="fade-down">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto eum
           excepturi, quo
           voluptas
           ad id recusandae reprehenderit minima, vero ipsum voluptatibus! Mollitia architecto fugiat deleniti ut,
           molestiae voluptate culpa suscipit.</p>
-        <div class="social-icons" data-aos="fade-right">
+        <div class="social-icons" data-aos-duration="150" data-aos="fade-down">
           <button class="btn-label" aria-label="linkedin" data-cooltipz-dir="bottom">
             <a class="social-icon" href="https://www.linkedin.com/in/rodrigoacsoares/" target="_blank"><i
                 class="fa-brands fa-linkedin"></i></a>
@@ -64,16 +64,16 @@
     </section>
     <hr class="hr-text" data-content="▼">
     <!-- Experience-->
-    <section class="resume-section print" id="experience" data-aos="fade-right">
+    <section class="resume-section print" id="experience" data-aos-duration="150" data-aos="fade-down">
       <div class="resume-section-content">
-        <h2 class="mb-5" data-aos="fade-right">Experiência</h2>
+        <h2 class="mb-5" data-aos-duration="150" data-aos="fade-down">Experiência</h2>
         <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="flex-grow-1">
-            <h3 class="mb-0" data-aos="fade-right">Cargo</h3>
-            <div class="subheading mb-3" data-aos="fade-right">Empresa</div>
-            <p data-aos="fade-right">Descrição.</p>
+            <h3 class="mb-0" data-aos-duration="150" data-aos="fade-down">Cargo</h3>
+            <div class="subheading mb-3" data-aos-duration="150" data-aos="fade-down">Empresa</div>
+            <p data-aos-duration="150" data-aos="fade-down">Descrição.</p>
           </div>
-          <div class="flex-shrink-0" data-aos="fade-right"><span class="text-primary">Data</span></div>
+          <div class="flex-shrink-0" data-aos-duration="150" data-aos="fade-down"><span class="text-primary">Data</span></div>
         </div>
       </div>
     </section>
@@ -81,82 +81,82 @@
     <!-- Education-->
     <section class="resume-section print" id="education">
       <div class="resume-section-content">
-        <h2 data-aos="fade-right" class="mb-5">formação</h2>
+        <h2 data-aos-duration="150" data-aos="fade-down" class="mb-5">formação</h2>
         <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="flex-grow-1">
-            <h3 data-aos="fade-right" class="mb-0">sistemas de informação</h3>
-            <div data-aos="fade-right" class="subheading mb-3">bacharelado</div>
+            <h3 data-aos-duration="150" data-aos="fade-down" class="mb-0">sistemas de informação</h3>
+            <div data-aos-duration="150" data-aos="fade-down" class="subheading mb-3">bacharelado</div>
             <div></div>
-            <p data-aos="fade-right">Instituto Federal de Minas Gerais - Campus Ouro Branco</p>
+            <p data-aos-duration="150" data-aos="fade-down">Instituto Federal de Minas Gerais - Campus Ouro Branco</p>
           </div>
-          <div data-aos="fade-right" class="flex-shrink-0"><span class="text-primary">setembro
+          <div data-aos-duration="150" data-aos="fade-down" class="flex-shrink-0"><span class="text-primary">setembro
               de 2022 - cursando</span></div>
         </div>
         <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="flex-grow-1">
-            <h3 data-aos="fade-right" class="mb-0">Mecânica</h3>
-            <div data-aos="fade-right" class="subheading mb-3">técnico integrado</div>
+            <h3 data-aos-duration="150" data-aos="fade-down" class="mb-0">Mecânica</h3>
+            <div data-aos-duration="150" data-aos="fade-down" class="subheading mb-3">técnico integrado</div>
             <div></div>
-            <p data-aos="fade-right">Instituto Federal de Minas Gerais - Campus Congonhas</p>
+            <p data-aos-duration="150" data-aos="fade-down">Instituto Federal de Minas Gerais - Campus Congonhas</p>
           </div>
-          <div data-aos="fade-right" class="flex-shrink-0"><span class="text-primary">fevereiro
+          <div data-aos-duration="150" data-aos="fade-down" class="flex-shrink-0"><span class="text-primary">fevereiro
               de 2018 - abril de 2022</span></div>
         </div>
       </div>
     </section>
     <hr class="hr-text" data-content="▼">
     <!-- Skills-->
-    <section class="resume-section print" id="skills" data-aos="fade-right">
+    <section class="resume-section print" id="skills" data-aos-duration="150" data-aos="fade-down">
       <div class="resume-section-content">
-        <h2 class="mb-5" data-aos="fade-right">Habilidades</h2>
-        <div class="subheading mb-3" data-aos="fade-right">Linguagens e ferramentas</div>
+        <h2 class="mb-5" data-aos-duration="150" data-aos="fade-down">Habilidades</h2>
+        <div class="subheading mb-3" data-aos-duration="150" data-aos="fade-down">Linguagens e ferramentas</div>
         <ul class="list-inline dev-icons">
-          <li class="list-inline-item" data-aos="fade-right">
+          <li class="list-inline-item" data-aos-duration="150" data-aos="fade-down">
             <button aria-label="HTML" data-cooltipz-dir="top">
               <i class="fab fa-html5 "></i>
             </button>
           </li>
-          <li class="list-inline-item" data-aos="fade-right">
+          <li class="list-inline-item" data-aos-duration="150" data-aos="fade-down">
             <button aria-label="CSS" data-cooltipz-dir="top">
               <i class="fab fa-css3-alt"></i>
           </li>
           </button>
-          <li class="list-inline-item" data-aos="fade-right">
+          <li class="list-inline-item" data-aos-duration="150" data-aos="fade-down">
             <button aria-label="JavaScript" data-cooltipz-dir="top">
               <i class="fab fa-js-square"></i>
             </button>
           </li>
-          <li class="list-inline-item" data-aos="fade-right">
+          <li class="list-inline-item" data-aos-duration="150" data-aos="fade-down">
             <button aria-label="NodeJS" data-cooltipz-dir="top">
               <i class="fab fa-node-js"></i>
             </button>
           </li>
-          <li class="list-inline-item" data-aos="fade-right">
+          <li class="list-inline-item" data-aos-duration="150" data-aos="fade-down">
             <button aria-label="SQL" data-cooltipz-dir="top">
               <i class="fab fa-solid fa-database"></i>
             </button>
           </li>
-          <li class="list-inline-item" data-aos="fade-right">
+          <li class="list-inline-item" data-aos-duration="150" data-aos="fade-down">
             <button aria-label="Git" data-cooltipz-dir="top">
               <i class="fab fa-brands fa-git-alt"></i>
             </button>
           </li>
         </ul>
-        <div class="subheading mb-3" data-aos="fade-right">Lorem</div>
+        <div class="subheading mb-3" data-aos-duration="150" data-aos="fade-down">Lorem</div>
         <ul class="fa-ul mb-0">
-          <li data-aos="fade-right">
+          <li data-aos-duration="150" data-aos="fade-down">
             <span class="fa-li"><i class="fas fa-check"></i></span>
             Lorem ipsum dolor sit amet
           </li>
-          <li data-aos="fade-right">
+          <li data-aos-duration="150" data-aos="fade-down">
             <span class="fa-li"><i class="fas fa-check"></i></span>
             Lorem ipsum dolor sit amet
           </li>
-          <li data-aos="fade-right">
+          <li data-aos-duration="150" data-aos="fade-down">
             <span class="fa-li"><i class="fas fa-check"></i></span>
             Lorem ipsum dolor sit amet
           </li>
-          <li data-aos="fade-right">
+          <li data-aos-duration="150" data-aos="fade-down">
             <span class="fa-li"><i class="fas fa-check"></i></span>
             Lorem ipsum dolor sit amet
           </li>
@@ -165,10 +165,10 @@
     </section>
     <hr class="hr-text" data-content="▼">
     <!-- Projects-->
-    <section class="resume-section" id="projects" data-aos="fade-right">
+    <section class="resume-section" id="projects" data-aos-duration="150" data-aos="fade-down">
       <div class="resume-section-content">
-        <h2 class="mb-5" data-aos="fade-right">Projetos</h2>
-        <p data-aos="fade-right">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque optio, veniam
+        <h2 class="mb-5" data-aos-duration="150" data-aos="fade-down">Projetos</h2>
+        <p data-aos-duration="150" data-aos="fade-down">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eaque optio, veniam
           mollitia voluptate odit
           quod quibusdam distinctio quasi eius temporibus reprehenderit ducimus in? Optio vel nam enim iure architecto.
         </p>
