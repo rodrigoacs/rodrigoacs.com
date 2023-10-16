7915d37cd43ef158ddd385c7687f272fe0d42dbd
Rodrigo <rodrigohths@gmail.com>
2023-10-15 21:32:19 -0300

    chore: /ch mudança visual

---
+++ resources/js/ch.js
@@ -0,0 +1,101 @@
+const previousDate = document.getElementById("previousDate")
+const nextDate = document.getElementById("nextDate")
+const currentDate = document.getElementById("currentDate")
+const addActivityButton = document.getElementById("add")
+const calculateButton = document.getElementById("calculate")
+const result = document.getElementById("result")
+
+let current = new Date()
+let lastTotalTime = 0
+
+currentDate.innerText = getFormattedDate(current)
+
+previousDate.addEventListener("click", () => changeDate(-1))
+nextDate.addEventListener("click", () => changeDate(1))
+addActivityButton.addEventListener("click", addActivity)
+
+setInterval(calculateTotal, 500)
+
+function changeDate(days) {
+  current.setDate(current.getDate() + days)
+  currentDate.innerText = getFormattedDate(current)
+}
+
+function getFormattedDate(date) {
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+}
+
+function getTaskTotal(initial, final) {
+  const today = new Date()
+  initial = parseTime(today, initial)
+  final = parseTime(today, final)
+  return (final - initial) / 60000
+}
+
+function parseTime(date, time) {
+  const [hours, minutes] = time.split(":")
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate(), hours, minutes).getTime()
+}
+
+function calculateTotal() {
+  const intervals = document.querySelectorAll(".interval")
+  let totalTime = 0
+
+  for (const interval of intervals) {
+    const initial = interval.children[0].children[0].value
+    const final = interval.children[0].children[1].value
+
+    if (initial && final) {
+      const taskTime = getTaskTotal(initial, final)
+      totalTime += taskTime
+    }
+  }
+
+  if (totalTime === lastTotalTime) {
+    return
+  }
+
+  lastTotalTime = totalTime
+
+  const totalFormattedTime = formatTime(totalTime)
+  if (totalTime === 0) {
+    return result.innerText = "Nenhuma atividade adicionada"
+  }
+
+  const formattedHours = totalFormattedTime[0].toString().padStart(2, '0') 
+  const formattedMinutes = totalFormattedTime[1].toString().padStart(2, '0')
+
+  result.innerText = `${formattedHours}:${formattedMinutes}`
+}
+
+function formatTime(time) {
+  const hours = parseInt(time / 60)
+  const minutes = time - hours * 60
+  return [hours, minutes]
+}
+
+function addActivity() {
+  const task = document.createElement("div")
+  const div = document.createElement("div")
+  task.classList.add("interval")
+
+  const startTime = createInputElement("time")
+  const endTime = createInputElement("time")
+  const description = createInputElement("text")
+  const totalTime = createInputElement("text", true)
+
+  div.appendChild(startTime)
+  div.appendChild(endTime)
+  div.appendChild(totalTime)
+  task.appendChild(div)
+  task.appendChild(description)
+
+  document.body.appendChild(task)
+}
+
+function createInputElement(type, readOnly = false) {
+  const input = document.createElement("input")
+  input.type = type
+  if (readOnly) input.readOnly = true
+  return input
+}
--- resources/views/ch.blade.php
+++ resources/views/ch.blade.php
@@ -2,134 +2,37 @@
 <html lang="pt-BR">
 
 <head>
-  <link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon-16x16.png">
-  <link rel="manifest" href="./assets/site.webmanifest">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
-  <style>
-    :root {
-      --background-body: #1f1f1f;
-      --background: #f0f0f0;
-      --background-alt: #1a242f;
-      --selection: #1c76c5;
-      --text-main: #f0f0f0;
-      --text-bright: #070707;
-      --text-muted: #a9b1ba;
-      --links: #41adff;
-      --focus: #0096bfab;
-      --border: #f0f0f0;
-      --code: #ffbe85;
-      --animation-duration: 0.1s;
-      --button-base: #f0f0f0;
-      --button-hover: #dadada;
-      --scrollbar-thumb: var(--button-hover);
-      --scrollbar-thumb-hover: rgb(0, 0, 0);
-      --form-placeholder: #a9a9a9;
-      --form-text: #070707;
-      --variable: #d941e2;
-      --highlight: #efdb43;
-    }
-
-    div {
-      display: flex;
-      justify-content: center;
-    }
-
-    input {
-      display: inline-block;
-    }
-
-    footer {
-      width: 100%;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-    }
-  </style>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="workedHours.js"></script>
+  <link rel="stylesheet" href="style.css">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+    rel="stylesheet">
   <title>calculadora de intervalos de tempo</title>
 </head>
 
-<body class="times">
-  <div>
-    <button onclick="addActivity()">+</button>
-    <button onclick="calculateAll()" id="calculate">calcular</button>
-  </div>
-  <div>
-    <p>&nbsp;</p>
-    <p id="result"></p>
-  </div>
-  <div class="interval">
-    <input type="time">
-    <input type="time">
-    <input type="text">
-    <input type="text" readonly></input>
+<body>
+  <div class="menu">
+    <div>
+      <button id="previousDate" disabled></button>
+      <span id="currentDate"></span>
+      <button id="nextDate" disabled></button>
+    </div>
+    <button id="add">+</button>
+    <span id="result">00:00</span>
   </div>
 
-  <footer>&nbsp;calculadora de intervalos de tempo v1.2</footer>
+  <div class="interval">
+    <div>
+      <input type="time">
+      <input type="time">
+      <input type="text" readonly></input>
+    </div>
+    <input type="text">
+  </div>
 </body>
 
-<script>
-  function calculateIndividual(initial, final) {
-    initial = new Date(`01/01/2023 ${initial}:00`).getTime()
-    final = new Date(`01/01/2023 ${final}:00`).getTime()
-
-    return (final - initial) / 60000
-  }
-
-  function calculateAll() {
-    let intervals = document.getElementsByClassName("interval")
-    let spentTime = 0
-    let formatedTime = []
-    for (let i = 0; i < intervals.length; i++) {
-      let initial = intervals[i].children[0].value
-      let final = intervals[i].children[1].value
-
-      if (!initial | !final) {
-        break
-      }
-
-      let spentIndividual = calculateIndividual(initial, final)
-      formatedTime = formatTime(spentIndividual)
-      intervals[i].children[3].value = `${formatedTime[0]} hora(s) e ${formatedTime[1]} minuto(s)`
-      spentTime += spentIndividual
-    }
-
-    formatedTime = formatTime(spentTime)
-    document.getElementById("result").innerText = `${formatedTime[0]} hora(s) e ${formatedTime[1]} minuto(s)`
-  }
-
-  function formatTime(time) {
-    let hours = parseInt(time / 60)
-    let minutes = time - hours * 60
-    return [hours, minutes]
-  }
-
-  function addActivity() {
-    let times = document.getElementsByClassName("times")
-    let div = document.createElement("div")
-    div.classList = "interval"
-    let inputTime = document.createElement("input")
-    inputTime.type = "time"
-    let inputTime2 = document.createElement("input")
-    inputTime2.type = "time"
-    let inputText = document.createElement("input")
-    inputText.type = "text"
-    let individualTime = document.createElement("input")
-    individualTime.type = "text"
-    individualTime.readOnly = true
-
-    div.appendChild(inputTime)
-    div.appendChild(inputTime2)
-    div.appendChild(inputText)
-    div.appendChild(individualTime)
-
-    times[times.length - 1].appendChild(div)
-  }
-</script>
+<script src="ch.js"></script>
 
 </html>
