4f6d22ed221dd736cdd8d2e5f0e21bcea25f397f
Rodrigo <rodrigohths@gmail.com>
2023-11-23 00:19:33 -0300

    fix: import desnecessario de fontes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,7 @@
   <title>rodrigoacs.com</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
   <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
 </head>
 
@@ -24,9 +22,7 @@
     </div>
     <div>
       <a id="change-language"></a>
-      <a id="change-theme"><img
-          src="./assets/new_moon_3d.png"
-          alt="" id="change-theme-img" style="width: 1.5rem;"></a>
+      <a id="change-theme"><img src="./assets/new_moon_3d.png" alt="" id="change-theme-img" style="width: 1.5rem;"></a>
     </div>
   </nav>
   <main class="ajax">
