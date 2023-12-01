a63362e211c488bc4f38b94a20b220ea57771230
Rodrigo <rodrigohths@gmail.com>
2023-12-01 15:49:35 -0300

    add: mudança na mensagem de 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -13,7 +13,8 @@
   <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
 </head>
 <body style="display: flex;height: 100vh;flex-direction: column;align-items: center;justify-content:center">
-  <p>404</p>
+  <h1>404</h1>
+  <p>you shall not pass</p>
   <a href="https://rodrigoacs.com/">cd ./</a>
 </body>
 </html>
